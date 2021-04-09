f5e74de70c3eff4746f1d974c98cf37f0c9fc328
AJRepo <AJRepo@users.noreply.github.com>
2021-04-09 13:59:05 -0500

    Add debug/verbose flags, code cleanup

--- docker_image_check.sh
+++ docker_image_check.sh
@@ -11,24 +11,53 @@ SECURITY_LEVEL=0
 SECURITY_CODES=()
 
 DRY_RUN='false'
+DEBUG='false'
+VERBOSE='false'
 
-function usage {
+# Function: usage() print usage.
+function usage() {
 	echo "Usage: $(basename "$0") -p " 2>&1
+	echo '   -d    Debug mode (print more)'
+	echo '   -v    Verbose mode (print more)'
 	echo '   -n    Dry Run (do not download)'
-	echo '   -d    Docker image to analize (nvida, ubuntu)'
+	echo '   -r    Root docker image to analize (nvida, ubuntu)'
 	exit 1
 }
 
+#Function print_v
+function print_v() {
+   local level=$1
+
+   case $level in
+      v) # Verbose
+      [[ "$VERBOSE" == 'true' ]] && echo -e "[VER] ${*:2}"
+      ;;
+      d) # Debug
+      [[ "$DEBUG" == 'true' ]] && echo -e "[DEB] ${*:2}"
+      ;;
+      e) # Error
+      echo -e "[ERR] ${*:2}"
+      ;;
+      w) # Warning
+      echo -e "[WAR] ${*:2}"
+      ;;
+      *) # Any other level
+      echo -e "[INF] ${*:2}"
+      ;;
+   esac
+}
+
+
 #Function: set_docker_param(): set the param based on docker root
-function set_docker_param {
+function set_docker_param() {
 	local known_docker_param='false'
 	local -n return_val=$1
 	local -n aa_valid_params=$2
 	local param_to_lookup=$3
 
-	echo "PARAM=$param_to_lookup"
-	echo "FOO=" "${aa_valid_params[@]}"
-	echo "BAR=" "${aa_valid_params[$param_to_lookup]}"
+	print_v d " PARAM_to_lookup=$param_to_lookup"
+	#echo "FOO='" "${aa_valid_params[@]}" "'"
+	#echo "BAR=" "${aa_valid_params[$param_to_lookup]}"
 
 	if [[ $param_to_lookup == "" ]]; then
 		echo "Docker param not set. Picking default"
@@ -43,11 +72,10 @@ function set_docker_param {
 	fi
 
 	if [[ $known_docker_param == 'false' ]]; then
-		echo "Unknown Docker Param. Exiting"
-		return_val='false'
-		exit 1
+		print_v d " Unknown Docker Param. Returning '' "
+		return_val=''
 	else
-		echo "Docker Param=$return_val"
+		print_v d " Docker Param=$return_val"
 	fi
 
 	return 0
@@ -55,66 +83,67 @@ function set_docker_param {
 
 # Function: pass in root and optionally tag, product for analysis
 # this_docker_root string: is required
-function setup_arrays {
-    local this_docker_root=$1
+function setup_docker_globals() {
+    local optarg_docker_root=$1
     local this_docker_tag=$2
 	local this_docker_product=''
+    local this_docker_root=''
 
-    local known_docker_root='false'
-
+	declare -A aIMAGE_ROOT
 	declare -A aIMAGE_TAG
 	declare -A aIMAGE_PRODUCT
 
+	aIMAGE_ROOT["ubuntu"]="ubuntu"
+	aIMAGE_ROOT["ubuntu20"]="ubuntu"
+	aIMAGE_ROOT["nvidia"]="nvidia"
+
 	aIMAGE_TAG["ubuntu"]="focal"
+	aIMAGE_TAG["ubuntu20"]="focal"
 	aIMAGE_TAG["nvidia"]="11.0-base"
 
 	aIMAGE_PRODUCT["ubuntu"]=""
 	aIMAGE_PRODUCT["nvidia"]="cuda"
 
-	echo "DEBUG: $this_docker_root"
+	print_v d " $this_docker_root with " "${aIMAGE_ROOT[@]}"
 
-	IMAGE_VENDORS=("nvidia" "ubuntu")
-	if [[ $this_docker_root == "" ]]; then
-		echo "Docker root not set. Defaulting to ubuntu"
-		this_docker_root="ubuntu"
-		known_docker_root='true'
-	else
-		for this_root in "${IMAGE_VENDORS[@]}"; do
-			if [[ $this_root == "$this_docker_root" ]]; then
-				known_docker_root='true'
-			fi
-		done
-	fi
-
-	if [[ $known_docker_root == 'false' ]]; then
-		echo "Unknown Docker Root. Exiting"
+	#set variable this_docker_tag
+	set_docker_param this_docker_root aIMAGE_ROOT "$optarg_docker_root"
+	if [[ $this_docker_root == '' ]]; then
+		echo "Error: No docker root set. Exiting 1"
 		exit 1
-	else
-		echo "Docker Root=$this_docker_root"
 	fi
 
+	#print_v d " about to lookup aIMAGE_PRODUCT"
+	#set variable this_docker_tag
 	set_docker_param this_docker_tag aIMAGE_TAG "$this_docker_root"
 
 	if [[ $this_docker_tag == 'false' ]]; then
-		echo "Docker tag not set. Used " "${aIMAGE_TAG[@]}" " Error: exiting"
+		echo "Error: Docker tag not set. Used " "${aIMAGE_TAG[@]}" " Error: exiting"
 		exit 1
 	else
-		echo "Docker Tag=$this_docker_tag"
+		print_v d " Docker Tag=$this_docker_tag"
 	fi
 
+	#set variable this_docker_product
+	print_v d " about to lookup aIMAGE_PRODUCT"
 	set_docker_param this_docker_product aIMAGE_PRODUCT "$this_docker_root"
 
 	if [[ $this_docker_product == 'false' ]]; then
-		echo "Docker product not set. Used " "${aIMAGE_PRODUCT[@]}" " Error: exiting"
+		echo "Error: Docker product not set. Used " "${aIMAGE_PRODUCT[@]}" " Error: exiting"
 		exit 1
 	else
-		echo "Docker product=$this_docker_product"
+		print_v d " Docker product=$this_docker_product"
 	fi
 
-	echo "THIS TAG = $this_docker_tag"
-	echo "THIS PRODUCT = $this_docker_product"
+	print_v v "THIS DOCKER ROOT = $this_docker_root"
+	print_v v "THIS DOCKER TAG = $this_docker_tag"
+	print_v v "THIS DOCKER PRODUCT = $this_docker_product"
 
-	DOCKER_IMAGE="$this_docker_product:$this_docker_tag"
+	THIS_DOCKER_ROOT="$this_docker_root"
+	THIS_DOCKER_TAG="$this_docker_tag"
+	THIS_DOCKER_PRODUCT="$this_docker_product"
+	DOCKER_IMAGE="$this_docker_root:$this_docker_tag"
+	print_v v "THIS DOCKER_IMAGE = $DOCKER_IMAGE"
 }
 
 #If called without args error out
@@ -124,67 +153,94 @@ if [[ ${#} -eq 0 ]]; then
 	exit 1
 fi
 
-optstring="nd:"
+DEBUG='false'
+VERBOSE='false'
+optstring="vndr:"
 while getopts ${optstring} arg; do
 	case ${arg} in
 	n)
 		DRY_RUN='true'
 	;;
 	d)
+		DEBUG='true'
+	;;
+	v)
+		VERBOSE='true'
+	;;
+	r)
 		DOCKER_ROOT="${OPTARG}"
-
 	;;
 	esac
 done
 
-echo "DOCKER_ROOT=$DOCKER_ROOT"
-
-setup_arrays "$DOCKER_ROOT"
-
-exit 0
-
+setup_docker_globals "$DOCKER_ROOT"
 
 #which image to analize
 i=1
 
 IMAGE_PRODUCTS=("cuda" "")
+
 #Declare Associative Array Variables for various sources
 declare -A aIMAGE_SOURCE_URL
 declare -A aIMAGE_SOURCE_HASHES
 declare -A aIMAGE_SOURCE_IMAGE
 aIMAGE_SOURCE_IMAGE["ubuntu:focal"]="ubuntu-focal-core-cloudimg-amd64-root.tar.gz"
+aIMAGE_SOURCE_IMAGE["nvidia:11.0-base"]=""
+
 aIMAGE_SOURCE_HASHES["ubuntu:focal"]="https://partner-images.canonical.com/core/focal/current/SHA256SUMS"
+#NVIDIA Doesn't have them
+aIMAGE_SOURCE_HASHES["nvidia:11.0-base"]=""
+
+# NVIDIA: See https://ngc.nvidia.com/catalog/containers/nvidia:cuda
+# NVIDIA: Uses Ubuntu for their core image. So looking at NVIDIA requires looking
+#         at the base Ubuntu packages and then NVIDIA deb packages.
 aIMAGE_SOURCE_URL["ubuntu:focal"]="https://partner-images.canonical.com/core/focal/current/${aIMAGE_SOURCE_IMAGE['ubuntu:focal']}"
+aIMAGE_SOURCE_URL["nvidia:11.0-base"]="other"
 
 
+this_source_url=""
+print_v d " about to run aIMAGE_SOURCE_URL"
+#set variable this_source_url (where we download the source docker image)
+set_docker_param this_source_url aIMAGE_SOURCE_URL "$DOCKER_IMAGE"
+
+print_v d "SOURCE URL= $this_source_url"
 
 
-if [[ ${IMAGE_PRODUCTS[$i]} != "" ]]; then
-	IMAGE_VENDOR_PRODUCT="${IMAGE_VENDORS[$i]}/${IMAGE_PRODUCTS[$i]}"
+if [[ $THIS_DOCKER_PRODUCT != "" ]]; then
+	IMAGE_VENDOR_PRODUCT="$THIS_DOCKER_ROOT/$THIS_DOCKER_PRODUCT"
 else
-	IMAGE_VENDOR_PRODUCT="${IMAGE_VENDORS[$i]}"
+	IMAGE_VENDOR_PRODUCT="$THIS_DOCKER_ROOT"
 fi
 
 #DOCKER_IMAGE set in setup_arrays
 #DOCKER_IMAGE="$IMAGE_VENDOR_PRODUCT:${IMAGE_TAGS[$i]}"
 
-echo "Checking $DOCKER_IMAGE"
-echo "Source Image Identified as: ${aIMAGE_SOURCE_IMAGE[${DOCKER_IMAGE}]}"
+print_v v "Checking $DOCKER_IMAGE"
+print_v v "Source Image Identified as: ${aIMAGE_SOURCE_IMAGE[${DOCKER_IMAGE}]}"
+print_v v "Source URL Identified as: ${aIMAGE_SOURCE_URL[${DOCKER_IMAGE}]}"
 
-if [[ ${aIMAGE_SOURCE_URL[$DOCKER_IMAGE]} != "" ]]; then
+if [[ ${aIMAGE_SOURCE_URL[$DOCKER_IMAGE]} == "other" ]]; then
+	echo "This docker image doesn't use their own core image, skipping this step"
+elif [[ ${aIMAGE_SOURCE_URL[$DOCKER_IMAGE]} != "" ]]; then
 	if [[ $DRY_RUN == 'false' ]]; then
 		wget -O /tmp/latest_docker_image.tgz ${aIMAGE_SOURCE_URL[$DOCKER_IMAGE]}
 	fi
 else
-	echo "Couldn't find vendor's source image. Exiting."
+	echo "Couldn't find vendor's source image file. Exiting."
 	exit 1
 fi
 
+
 if [[ ${aIMAGE_SOURCE_HASHES[$DOCKER_IMAGE]} != "" ]]; then
-	echo "DEBUG 1: Uncomment to activate ${aIMAGE_SOURCE_IMAGE[$DOCKER_IMAGE]}"
-	#wget -O /tmp/image_hashes.txt ${aIMAGE_SOURCE_HASHES[$DOCKER_IMAGE]}
-	if ! grep ${aIMAGE_SOURCE_IMAGE[$DOCKER_IMAGE]} /tmp/image_hashes.txt | awk '{print $1, "/tmp/latest_docker_image.tgz"}' | sha256sum --check --; then
-		echo "Download of Vendor original source image failed hash check. Exiting"
+	print_v d "DEBUG 1: Uncomment to activate ${aIMAGE_SOURCE_IMAGE[$DOCKER_IMAGE]}"
+	if [[ $DRY_RUN == 'false' ]]; then
+		wget -O /tmp/docker_image_hashes.txt ${aIMAGE_SOURCE_HASHES[$DOCKER_IMAGE]}
+	else
+		print_v v "Skipping new download of docker_iamge_hashes"
+	fi
+	if ! grep ${aIMAGE_SOURCE_IMAGE[$DOCKER_IMAGE]} /tmp/docker_image_hashes.txt | awk '{print $1, "/tmp/latest_docker_image.tgz"}' | sha256sum --check --; then
+		echo "Download of Vendor original source image failed hash check."
+		echo "Failed hash check means also failed security audit. Exiting"
 		exit 1
 	fi
 else
@@ -193,7 +249,7 @@ else
 	SECURITY_CODES+=("Image not signed by vendor")
 fi
 
-exit
+exit 0
 
 #Get image to analize
 if ! sudo DOCKER_CONTENT_TRUST=1 docker pull "$DOCKER_IMAGE"; then
